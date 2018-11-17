2654630e790918880cb4705d2f8b39541349eef7
zeratul94 <tomi.avramovski@gmail.com>
2018-11-17 20:29:24 +0100

    coffee shop

---
+++ coffee-shop/assets/js/global.js
@@ -0,0 +1,12 @@
+// Homepage brand slider
+
+var slider = tns({
+    container: '.brand-slider',
+    slideBy: 'page',
+    autoplay: true,
+    items: 6,
+    mouseDrag: true,
+    speed: 400,
+    controls: false,
+    autoplayHoverPause: true,
+});
